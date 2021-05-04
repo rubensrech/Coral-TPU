4a41a02d96cd713b5465b78aee945f08c991dda3
Rubens Rech <rubensch@icloud.com>
2021-05-03 23:39:42 -0300

    generate random inputs

--- elementary-ops/prepare_input_for_model.py
+++ elementary-ops/prepare_input_for_model.py
@@ -15,19 +15,46 @@ INPUTS_DIR = f"{Path(__file__).parent}/inputs"
 def get_input_image_name(image_filename):
     return Path(image_filename).stem
 
-def get_output_image_name(model_file, image_file):
-    img_name = get_input_image_name(image_file)
+def get_output_name(model_file, image_file=None):
+    if image_file is not None:
+        out_desc = get_input_image_name(image_file)
+        ext = ".bmp"
+    else:
+        out_desc = "rand"
+        ext = ".npy"
+
     op = util.get_op_from_model_name(model_file)
     opStr = op.value
     dims = util.get_dims_from_model_name(model_file)
     dimsStr = "_".join(map(str, dims))
-    ext = "bmp"
-    return f"{INPUTS_DIR}/{img_name}-{op.value}_{dimsStr}.{ext}"
+    
+    return f"{INPUTS_DIR}/{out_desc}-{op.value}_{dimsStr}{ext}"
+
+def generate_from_input_image(model_file, image_file, output_size, op):
+    img = Image.open(image_file)
+    img = img.resize(output_size)
+
+    if op == Operation.Conv2d:
+        img = img.convert('L')
+
+    output_shape = np.array(img).shape
+    output_file = get_output_name(model_file, image_file)
+
+    img.save(output_file)
+
+    print(f'Output image saved to `{output_file}` with dimensions {output_shape}')
+
+def generate_random_input(model_file, size, op):
+    rand_input = np.random.randint(0, 255, size, dtype=np.uint8)
+    output_file = get_output_name(model_file)
+    np.save(output_file, rand_input)
+    print(f'Random input saved to `{output_file}` with dimensions {size}')
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-I', '--input', required=True,
-                        help='Path to the input image (.jpg,.png,...)')
+    parser.add_argument('-I', '--input', required=False, default=None,
+                        help='Path to the input image (.jpg,.png,...)\
+                              If not available, random input will be generated')
     parser.add_argument('-M', '--model', required=True,
                         help='Path to the model file that will receive the BMP image as input')
     args = parser.parse_args()
@@ -38,20 +65,12 @@ def main():
     op = util.get_op_from_model_name(model_file)
     dims = util.get_dims_from_model_name(model_file)
 
-    outImgSize = dims[1:3]
+    output_size = dims[1:3]
 
-    img = Image.open(image_file)
-    img = img.resize(outImgSize)
-
-    if op == Operation.Conv2d:
-        img = img.convert('L')
-
-    outImgShape = np.array(img).shape
-    outImgFile = get_output_image_name(model_file, image_file)
-
-    img.save(outImgFile)
-
-    print(f'Output image saved to `{outImgFile}` with dimensions {outImgShape}')
+    if image_file is not None:
+        generate_from_input_image(image_file, output_size, op)
+    else:
+        generate_random_input(model_file, output_size, op)
 
 if __name__ == "__main__":
     main()
