05593212ee1fe420e94ab2b0e5f613989571db10
Rubens Rech <rubensch@icloud.com>
2021-05-05 21:47:30 -0300

    improve prepare input script

--- elementary-ops/prepare_input_for_model.py
+++ elementary-ops/prepare_input_for_model.py
@@ -15,39 +15,46 @@ INPUTS_DIR = f"{Path(__file__).parent}/inputs"
 def get_input_image_name(image_filename):
     return Path(image_filename).stem
 
-def get_output_name(model_file, image_file=None):
+def get_output_name(model_file, out_ext, image_file=None):
     if image_file is not None:
         out_desc = get_input_image_name(image_file)
-        ext = ".bmp"
     else:
         out_desc = "rand"
-        ext = ".npy"
 
     op = util.get_op_from_model_name(model_file)
     opStr = op.value
     dims = util.get_dims_from_model_name(model_file)
     dimsStr = "_".join(map(str, dims))
     
-    return f"{INPUTS_DIR}/{out_desc}-{op.value}_{dimsStr}{ext}"
+    return f"{INPUTS_DIR}/{out_desc}-{op.value}_{dimsStr}.{out_ext}"
 
-def generate_from_input_image(model_file, image_file, output_size, op):
+def generate_from_input_image(model_file, image_file, output_size, op, out_ext):
     img = Image.open(image_file)
     img = img.resize(output_size)
 
     if op == Operation.Conv2d:
         img = img.convert('L')
 
-    output_shape = np.array(img).shape
-    output_file = get_output_name(model_file, image_file)
-
-    img.save(output_file)
+    output_file = get_output_name(model_file, out_ext, image_file)
+    output_arr = np.array(img)
+    output_shape = output_arr.shape
+    
+    if out_ext == "bmp":
+        img.save(output_file)
+    elif out_ext == "npy":
+        np.save(output_file, output_arr)
 
     print(f'Output image saved to `{output_file}` with dimensions {output_shape}')
 
-def generate_random_input(model_file, size, op):
+def generate_random_input(model_file, size, op, out_ext):
     rand_input = np.random.randint(0, 255, size, dtype=np.uint8)
-    output_file = get_output_name(model_file)
-    np.save(output_file, rand_input)
+    output_file = get_output_name(model_file, out_ext)
+
+    if out_ext == "bmp":
+        Image.fromarray(rand_input).save(output_file)
+    elif out_ext == "npy":
+        np.save(output_file, rand_input)
+
     print(f'Random input saved to `{output_file}` with dimensions {size}')
 
 def main():
@@ -57,20 +64,24 @@ def main():
                               If not available, random input will be generated')
     parser.add_argument('-M', '--model', required=True,
                         help='Path to the model file that will receive the BMP image as input')
+    parser.add_argument('--npy', required=False, action='store_true', default=False,
+                        help='Wether the output file should be a .NPY (Numpy file)')             
     args = parser.parse_args()
 
     model_file = args.model
     image_file = args.input
+    npy_out = args.npy
 
     op = util.get_op_from_model_name(model_file)
     dims = util.get_dims_from_model_name(model_file)
 
     output_size = dims[1:3]
+    output_ext = "npy" if npy_out else "bmp"
 
     if image_file is not None:
-        generate_from_input_image(model_file, image_file, output_size, op)
+        generate_from_input_image(model_file, image_file, output_size, op, output_ext)
     else:
-        generate_random_input(model_file, output_size, op)
+        generate_random_input(model_file, output_size, op, output_ext)
 
 if __name__ == "__main__":
     main()
