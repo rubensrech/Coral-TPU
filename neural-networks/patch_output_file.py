01436ac3d0e1211ca362fde953b47690b06dcb2b
Rubens Rech <rubensch@icloud.com>
2021-05-14 21:53:10 -0300

    patch out file

---
+++ neural-networks/patch_output_file.py
@@ -0,0 +1,44 @@
+import numpy as np
+import argparse
+from src.utils import common
+
+def patch_out_file(out_file, error_descriptors):
+    out_data = common.load_tensors_from_file(out_file)
+
+    for err in error_descriptors:
+        position = err.get('position')
+        count = err.get('count', 1)
+        tensorName = err.get('tensor')
+        
+        if type(out_data[tensorName]) is int:
+            randVal = np.random.randint(0, 255)
+            prevVal = out_data[err.get('tensor')]
+            out_data[err.get('tensor')] = randVal
+            print(f"Patch (tensor: {err.get('tensor')}, position: 0, prevVal: {prevVal}, newVal: {randVal})")
+        else:
+            for i in range(count):
+                maxLinIdx = np.product(out_data[tensorName].shape)
+                randLinPos = np.random.randint(maxLinIdx)
+                randVal = randVal = np.random.rand() if out_data[tensorName].dtype == 'float32' else np.random.randint(0, 255)
+                prevVal = out_data[tensorName].ravel()[randLinPos]
+                out_data[tensorName].ravel()[randLinPos] = randVal
+                position = np.unravel_index(randLinPos, out_data[tensorName].shape)
+                print(f"Patch (tensor: {err.get('tensor')}, position: {position}, prevVal: {prevVal}, newVal: {randVal})")
+
+    np.save(out_file, out_data)
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('out_file', help='Path to file to be patched')
+    args = parser.parse_args()
+
+    patch_out_file(args.out_file, [
+        { 'tensor': 'boxes', 'count': 5 },
+        { 'tensor': 'class_ids', 'count': 3 },
+        { 'tensor': 'scores', 'count': 1 },
+        { 'tensor': 2, 'count': 10 },
+        { 'tensor': 'count', 'count': 1 },
+    ])
+
+if __name__ == '__main__':
+    main()
