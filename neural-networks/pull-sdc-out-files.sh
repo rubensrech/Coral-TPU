958dfc258c09d500dc709842cae26c67366c74dc
Rubens Rech <rubensch@icloud.com>
2021-05-22 18:31:42 -0300

    improve pull sdc outs

--- neural-networks/pull-sdc-out-files.sh
+++ neural-networks/pull-sdc-out-files.sh
@@ -1,7 +1,12 @@
-PERIOD_IN_MINS=10
+PERIOD_IN_MINS=5
 
 while true;
 do
+    SDC_OUT_FILES_BEFORE=$(ls -l outputs | wc -l)
     rsync -vzP -e ssh rasp-uk-tunnel:Coral-TPU/neural-networks/outputs/* ./outputs/
+    SDC_OUT_FILES_AFTER=$(ls -l outputs | wc -l)
+    NEW_SDC_OUT_FILES=$((SDC_OUT_FILES_AFTER - SDC_OUT_FILES_BEFORE))
+    echo "Total SDC output files: $SDC_OUT_FILES_AFTER"
+    echo "New SDC output files: $NEW_SDC_OUT_FILES"
     sleep $((PERIOD_IN_MINS * 60))
 done
