43c6fc1704224d0042e72684872839c533c14860
Rubens Rech <rubensch@icloud.com>
2021-06-06 13:35:22 -0300

    fix pull sdcs: too many args

--- neural-networks/pull-sdc-out-files.sh
+++ neural-networks/pull-sdc-out-files.sh
@@ -1,10 +1,12 @@
 PERIOD_IN_MINS=5
 
+cd outputs/
+
 while true;
 do
-    SDC_OUT_FILES_BEFORE=$(ls -l outputs | wc -l)
-    rsync -vzP -e ssh rasp-uk-tunnel:Coral-TPU/neural-networks/outputs/* ./outputs/
-    SDC_OUT_FILES_AFTER=$(ls -l outputs | wc -l)
+    SDC_OUT_FILES_BEFORE=$(ls -l | wc -l)
+    rsync -vzP -e ssh rasp-uk-tunnel:Coral-TPU/neural-networks/outputs/* ./
+    SDC_OUT_FILES_AFTER=$(ls -l | wc -l)
     NEW_SDC_OUT_FILES=$((SDC_OUT_FILES_AFTER - SDC_OUT_FILES_BEFORE))
     echo "Total SDC output files: $SDC_OUT_FILES_AFTER"
     echo "New SDC output files: $NEW_SDC_OUT_FILES"
